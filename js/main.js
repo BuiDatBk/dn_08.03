1c8bd0a674e2ccc4dd0cbd62eff654b54f11fa88
BuiDatBk <118350748+BuiDatBk@users.noreply.github.com>
2023-03-06 10:51:11 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -22,7 +22,7 @@ function firstQuestion(){
     Swal.fire({
         title: CONFIG.introTitle,
         text: CONFIG.introDesc,
-        imageUrl: 'img/logi.gif',
+        imageUrl: 'img/dn.jpg',
         imageWidth: 300,
         imageHeight: 300,
         background: '#fff url("img/iput-bg.jpg")',
